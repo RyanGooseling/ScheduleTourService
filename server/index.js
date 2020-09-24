4d9bbcd5964476abc757ca5a63b9f0d4dcf199ec
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-09-24 15:35:33 -0700

    Initialization of package.json, webpack, and babel

--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3004;
 
-// Static files to be passed to front end
-// Remember this is FRONT END capstone
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
