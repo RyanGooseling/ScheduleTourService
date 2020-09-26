67f066fceb38238ded10024de1366214fc7c8237
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-09-25 20:03:17 -0700

    Instantiate a React render

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+import DateCarousel from './components/DateCarousel.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      dates: '',
+      tourType: 'In-person',
     };
   }
 
+  componentDidMount() {
+    axios({
+      method: 'get',
+      url: '/'
+    })
+      .then((newData) => {
+        this.setState({
+          dates: newDates
+        });
+      })
+      .catch((err) => {
+        console.log('Error', err);
+      });
+  }
+
   render() {
-    return;
+    return (
+      <div>
+        <div>
+          <DateCarousel/>
+        </div>
+        <div>
+          <Schedule/>
+        </div>
+      </div>
+    );
   }
 }
 
