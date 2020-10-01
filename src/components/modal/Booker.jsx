5048f0eaa168aa953286f40e16195f9a89f6b2dc
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-01 13:03:24 -0700

    Implement styled components

--- src/components/modal/Booker.jsx
+++ src/components/modal/Booker.jsx
@@ -3,20 +3,35 @@ import React from 'react';
 
 const Booker = (props) => {
 
+  const Modal = styled.div`
+  width: 500px;
+  background: white;
+  border: 1px solid #ccc;
+  transition: 1.1s ease-out;
+  box-shadow: -2rem 2rem 2rem rgba(black, 0.2);
+  filter: blur(0);
+  transform: scale(1);
+  opacity: 1;
+  visibility: visible;
+  `;
+
+  const Content = styled.div`
+    padding: 1rem;
+  `;
+
   if (!props.modal) {
     return null;
   } else {
     return (
-
-      <div class="modal" id="modal">
+      <Modal id="modal">
         <h2>Let's Book</h2>
-        <div class="content">{'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis deserunt corrupti, ut fugit magni qui quasi nisi amet repellendus non fuga omnis a sed impedit explicabo accusantium nihil doloremque consequuntur.'}</div>
+        <Content>{'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis deserunt corrupti, ut fugit magni qui quasi nisi amet repellendus non fuga omnis a sed impedit explicabo accusantium nihil doloremque consequuntur.'}</Content>
         <div class="actions">
           <button class="toggle-button" onClick={() => props.onClose()}>
             close
           </button>
         </div>
-      </div>
+      </Modal>
     );
   }
 };
