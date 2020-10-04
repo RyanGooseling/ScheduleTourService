60901f0ffc8f0fa67c14d97fb8bde7ea3c9f840c
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-04 13:34:36 -0700

    Highlightof the timeWindow buttons

--- src/components/modalWindow/RightArrowBooker.jsx
+++ src/components/modalWindow/RightArrowBooker.jsx
@@ -2,13 +2,9 @@ import React, {useState} from 'react';
 import styled from 'styled-components';
 
 const RightArrowBooker = (props) => {
-  console.log(props.activeIndex);
-  console.log(props.openings);
-  console.log((props.activeIndex < props.openings.length - 3));
 
   const renderArrow = function() {
     if (props.activeIndex < props.openings.length - 3) {
-      console.log('should be here');
       return '>';
     } else {
       return '';
