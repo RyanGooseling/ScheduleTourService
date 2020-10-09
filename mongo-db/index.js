a9220f885071a6e23df9167fcf5ddaa811b7edcd
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-09 12:07:31 -0700

    Update for git ignore

--- mongo-db/index.js
+++ mongo-db/index.js
@@ -1,6 +1,6 @@
+const mongoUri = require('../client/dist/funFile.js');
 const mongoose = require('mongoose');
 const IP = '54.193.202.190';
-const mongoUri = 'mongodb+srv://akilduff:bluefin@bluefin.cgkmq.mongodb.net/bluefin-scheduletour?retryWrites=true&w=majority';
 
 mongoose.connect(mongoUri);
 const db = mongoose.connection;
