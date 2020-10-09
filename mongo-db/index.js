c3404bbdb796c708ce4c196ecf63b9c7ef2487e5
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-08 20:35:02 -0700

    Update to db to include IP

--- mongo-db/index.js
+++ mongo-db/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/bluefin-scheduletour';
+const IP = '54.193.202.190';
+const mongoUri = `mongodb://${IP}/bluefin-scheduletour`;
 
 mongoose.connect(mongoUri);
 const db = mongoose.connection;
