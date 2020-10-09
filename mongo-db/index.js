7371cf86664e58944c04bbe5d9a8e90c3a492a7b
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-09 12:00:44 -0700

    Update mongo uri typo

--- mongo-db/index.js
+++ mongo-db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const IP = '54.193.202.190';
-const mongoURi = 'mongodb+srv://akilduff:bluefin@bluefin.cgkmq.mongodb.net/bluefin-scheduletour?retryWrites=true&w=majority';
+const mongoUri = 'mongodb+srv://akilduff:bluefin@bluefin.cgkmq.mongodb.net/bluefin-scheduletour?retryWrites=true&w=majority';
 
 mongoose.connect(mongoUri);
 const db = mongoose.connection;
