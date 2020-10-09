e38bcb8e4cbf669e0ad4aa45514862072a81146d
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-09 11:57:38 -0700

    Set up for mongodb Atlas

--- mongo-db/index.js
+++ mongo-db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const IP = '54.193.202.190';
-const mongoUri = `mongodb://${IP}/bluefin-scheduletour`;
+const mongoURi = 'mongodb+srv://akilduff:bluefin@bluefin.cgkmq.mongodb.net/bluefin-scheduletour?retryWrites=true&w=majority';
 
 mongoose.connect(mongoUri);
 const db = mongoose.connection;
